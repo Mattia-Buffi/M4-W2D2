70425255d05b68876bcf249b2431908e277cd976
Mattia Buffello <buffello@me.com>
2024-02-08 23:33:31 +0100

    ricerca

--- script.js
+++ script.js
@@ -19,20 +19,35 @@ fetch('https://api.pexels.com/v1/search?query=random',{method:'GET',headers:{aut
     })
     .catch((err)=>console.log('Error find: ',err));
 
+// Event listener 
 
-// let nodeLink=document.getElementsByClassName('nav-link');
-// console.log(nodeLink);
+let nodeLink=document.getElementsByClassName('nav-link');
+console.log(nodeLink);
 // nodeLink.forEach((node)=>{
+//     console.log(node);
 //     node.addEventListener('click',(event)=>{
-//         console.log(event);
+//         console.log(event.target.innerText)
 //     })
-// })
+    
+// });
+for (const iterator of nodeLink) {
+    iterator.addEventListener('click',(event)=>{
+        changeCard(event.target.innerText);
+    })
+}
+//event ricerca
+let btnSearch=document.getElementById('searchPin');
+btnSearch.addEventListener('click',()=>{
+    let inputText=document.getElementById('searchText').value;
+    document.getElementById('searchText').value='';
+    changeCard(inputText);
+})
 
 function changeCard(query){
     fetch('https://api.pexels.com/v1/search?query='+query,{method:'GET',headers:{authorization:'M7HCP0XjhyguqNF5EyfnMSaXfGj6srj3UfkSAeocTGxy6VSrIxyG6Z5O'}})
     .then((Response)=>Response.json())
     .then((json)=>{
-        document.getElementById('mainTitle').innerText=query;
+        document.getElementById('mainTitle').innerText=query.toUpperCase();
         let mainNode=document.getElementsByClassName('pinChange');
         json.photos.forEach((foto,index)=>{
             mainNode[index].innerHTML=`<img src=${foto.src.large} class="card-img" alt=${foto.alt}><div class="card-img-overlay h-100 w-100 picInfo"><h5 class="card-title">PH: ${foto.photographer}</h5></div>`;
